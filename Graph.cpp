39d0e6726123163db25ee5674964ff7607487a24
Madhav-Goel12 <goelmadhav2006@gmail.com>
2026-09-25 18:20:22 +0000

    Update Graph.h and Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,77 +1,38 @@
 #include "Graph.h"
-#include <iostream>
 
-using namespace std;
-
-// Constructor
 Graph::Graph(int totalLocations) 
 {
     adjList.resize(totalLocations);
 }
-// Add a road to the adjacency list
-void Graph::addRoad(const Road& road) 
+void Graph::addRoad(Road road) 
 {
     int source = road.getSrc();
-    if (source >= 0 &&
-        source < static_cast<int>(adjList.size())) {
 
+    if (source >= 0 && source < adjList.size()) {
         adjList[source].push_back(road);
     }
 }
-// Display the complete adjacency list
-void Graph::showGraph() const 
+void Graph::showGraph() 
 {
-    cout << "\n=============== ADJACENCY LIST ===============" << endl;
-    for (int i = 0;i < static_cast<int>(adjList.size());i++) 
-         {
+    cout << "\n     ADJACENCY LIST      " << endl;
+
+    for (int i = 0; i < adjList.size(); i++) 
+    {
         cout << "Location " << i << " : ";
-        if (adjList[i].empty()) {
-            cout << "No outgoing roads";
+        if (adjList[i].empty()) 
+        {
+            cout << "   No outgoing roads   ";
         }
-        else {
-            for (int j = 0;
-                 j < static_cast<int>(adjList[i].size());
-                 j++) {
-
+        else 
+        {
+            for (int j = 0; j < adjList[i].size(); j++) 
+            {
                 cout << adjList[i][j].getDest();
-
-                if (j != static_cast<int>(adjList[i].size()) - 1) {
+                if (j != adjList[i].size() - 1) {
                     cout << " -> ";
                 }
             }
         }
-
         cout << endl;
     }
-}
-// Display all roads going out from one location
-void Graph::showOutgoingRoads(int locationId) const 
-{
-    if (locationId < 0 ||
-        locationId >= static_cast<int>(adjList.size())) 
-        {
-        cout << "Invalid location ID." << endl;
-        return;
-    }
-    cout << "\nRoads from Location "
-         << locationId << ":" << endl;
-    if (adjList[locationId].empty()) {
-        cout << "No outgoing roads." << endl;
-        return;
-    }
-    for (const Road& road : adjList[locationId]) {
-        road.printRoad();
-    }
-}
-// Return number of locations
-int Graph::getNumberOfLocations() const
- {
-
-    return static_cast<int>(adjList.size());
-}
-// Return roads connected to a location
-const vector<Road>& Graph::getNeighbors(int locationId) const
- {
-
-    return adjList[locationId];
 }
--- Graph.h
+++ Graph.h
@@ -1,30 +1,17 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <iostream>
 #include <vector>
+#include "Road.h"
 using namespace std;
 class Graph 
 {
 private:
     vector<vector<Road>> adjList;
 public:
-    // Constructor
     Graph(int totalLocations);
-
-    // Add a road to the graph
-    void addRoad(const Road& road);
-
-    // Display complete adjacency list
-    void showGraph() const;
-
-    // Display roads going out from one location
-    void showOutgoingRoads(int locationId) const;
-
-    // Return number of locations
-    int getNumberOfLocations() const;
-
-    // Get roads connected to a location
-    const vector<Road>& getNeighbors(int locationId) const;
+    void addRoad(Road road);
+    void showGraph();
 };
-
 #endif
